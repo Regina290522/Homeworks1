2037c8c3ec48c290436fb1f9e85b0527ad62f3dd
Regina290522 <reginafaizova579@gmail.com>
2024-10-05 14:04:06 +0300

    Lesson2

---
+++ Lesson2/lesson_2_task_2.py
@@ -0,0 +1,12 @@
+def is_year_leap(year):
+    if year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+year = 2022
+
+
+leap_year = is_year_leap(year)
+print(f"год{year}: {leap_year}")
