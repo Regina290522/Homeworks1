1de23f56a68845e9d65db6edbd8fd417c3151812
Regina290522 <reginafaizova579@gmail.com>
2024-11-07 11:03:27 +0300

    Lesson6

--- lesson_6_task_2.py
+++ lesson_6_task_2.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/textinput")
@@ -14,7 +15,10 @@ search_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, '#updatingButton')))
 search_button.click()
 
-element = WebDriverWait(driver, 10).until(
+try:
+    WebDriverWait(driver, 10).until(
         EC.text_to_be_present_in_element((By.CSS_SELECTOR,'#updatingButton'), "SkyPro"))
-
-driver.quit()
+    print("Кнопка была переименована успешно.")
+except TimeoutException:
+    print("Кнопка не была переименована.")
+    driver.quit()
