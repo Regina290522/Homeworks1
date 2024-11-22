17f7577e30a8bd76a2b51df286721d8d138b1b20
Regina290522 <reginafaizova579@gmail.com>
2024-11-22 10:33:28 +0300

    Lesson7

--- Lesson7/CalcPage.py
+++ Lesson7/CalcPage.py
@@ -1,17 +1,30 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pages.MainPage import MainPage
-from pages.CalcPage import CalcPage
-from pages.CartPage import CartPage
+from time import sleep
+class MainPage:
 
-def test_cart_counter():
-    browser = webdriver.Chrome()
-    main_page = MainPage(browser)
-    main_page.set_cookie_policy()
-    main_page.serch("Python")
+    def __init__(self, driver):
+        self._driver = driver
+        self._driver.get('https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html')
+        self._driver.implicitly_wait(10)
+        self._driver.maximize_window()
 
-    result_page = ResultPage(browser)
-    # result_page.switch_to_table()  #Используем только в видео
-    to_be = result_page.add_books()
+    def time(self, term):
+        time = self._driver.find_element(By.CSS_SELECTOR, 'input#delay')
+        time.clear()
+        time.send_keys(term)
+
+    def count(self):
+
+        self._driver.find_element(By.XPATH, "//span[text()='7']").click()
+        self._driver.find_element(By.XPATH, "//span[text()='+']").click()
+        self._driver.find_element(By.XPATH, "//span[text()='8']").click()
+        self._driver.find_element(By.XPATH, "//span[text()='=']").click()
+
+        WebDriverWait(self._driver, 50, 0.5).until(
+            EC.text_to_be_present_in_element(( By.CSS_SELECTOR, ".screen"), "15"))
+
+    def result(self):
+        res = self._driver.find_element(By.CSS_SELECTOR, ".screen").text
+        return int(res)
---
+++ Lesson7/test_calk.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from CalcPage import *
+
+
+def test_calc():
+    browser = webdriver.Chrome()
+
+    main_page = MainPage(browser)
+    main_page.time("45")
+    main_page.count()
+    to_be = main_page.result()
+
+    assert to_be == 15
+    browser.quit()
