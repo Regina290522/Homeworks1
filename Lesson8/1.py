77da00b5058cf9d020156ffbd3e8111c3ff65b5b
Regina290522 <reginafaizova579@gmail.com>
2024-12-02 10:36:25 +0300

    Lesson7

---
+++ Lesson8/1.py
@@ -0,0 +1,60 @@
+import requests
+
+token = 'QlHtq-S8ql4h8QE6HWFKayh9oDyLw8HNIpcv5MJ0Y6jE8DStqvqgmMlNNUKm-d7G'
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
+}
+
+def test_get_progects():
+    url = 'https://ru.yougile.com/api-v2/projects'
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200
+    assert 'content' in response.json()
+
+def test_create_progects():
+    url = 'https://ru.yougile.com/api-v2/projects'
+    body = {"title": "Домашнее задание №8"}
+    response = requests.post(url, headers=headers,json=body)
+
+    assert response.status_code == 201
+    assert 'id' in response.json()
+
+def test_modifite_progects():
+    url = 'https://ru.yougile.com/api-v2/projects'
+    body = {"title": "Домашнее задание №8"}
+    response = requests.post(url, headers=headers, json=body)
+    id=response.json()["id"]
+    url = f'https://ru.yougile.com/api-v2/projects/{id}'
+    body = {"title": "Обновленное название"}
+    response = requests.put(url, headers=headers, json=body)
+
+    assert response.status_code == 200
+    assert 'id' in response.json()
+
+def test_get_progect_by_id():
+    url = 'https://ru.yougile.com/api-v2/projects'
+    body = {"title": "Домашнее задание №8"}
+    response = requests.post(url, headers=headers, json=body)
+    id = response.json()["id"]
+    url = f'https://ru.yougile.com/api-v2/projects/{id}'
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200
+    assert 'id' in response.json()
+
+def test_get_progects_negative():
+    url = 'https://ru.yougile.com/api-v2/projects'
+    response = requests.get(url)
+
+    assert response.status_code == 401
+
+def test_create_progects_negative():
+    url = 'https://ru.yougile.com/api-v2/projects'
+    body = {}
+    response = requests.post(url, headers=headers,json=body)
+
+    assert response.status_code == 400
+
+
